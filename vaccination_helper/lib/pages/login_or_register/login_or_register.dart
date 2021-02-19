d059429173c2ed218fdc99b3029d6b7bf604fd13
Ruben Zsidó <zsruben03@example.com>
2021-02-19 18:41:34 +0100

    animated button

--- vaccination_helper/lib/pages/login_or_register/login_or_register.dart
+++ vaccination_helper/lib/pages/login_or_register/login_or_register.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
 
 
-class Loading extends StatelessWidget {
+class Button extends StatefulWidget {
+  @override
+  _ButtonState createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  double _scale;
+  @override
+  void initState() {
+   _controller = AnimationController(
+     vsync: this,
+     duration: Duration(milliseconds: 200),
+     lowerBound: 0.0,
+     upperBound: 0.1
+   )..addListener(() {setState(() {
+
+   });});
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(child: Container(),),
+    _scale = 1-_controller.value;
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('tap on the button below'),
+            SizedBox(height: 20.0,),
+            Center(
+              child: GestureDetector(
+                onTapDown: _onTapDown,
+                  onTapUp: _onTapUp,
+              child: Transform.scale(
+                  scale: _scale,
+              child: _animatedButtonUi,),),
+            )
+          ],
+        ),
+      ),
     );
   }
+  Widget get _animatedButtonUi => Container(
+    height: 70,
+    width: 300,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+
+      color: Colors.cyan),
+    child: Center(
+      child: Text("Tap",
+        style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          fontSize: 30),
+      ),
+    )
+    );
+  void _onTapDown(TapDownDetails details){
+    _controller.forward();
+
+  }
+  void _onTapUp(TapUpDetails details){
+    _controller.reverse();
+
+  }
+
 }
