8a6b3379e50b304d691b4e6a068b990a8ce4ff57
Balázs Blasek <blasek.balazs@gmail.com>
2021-02-19 18:51:37 +0100

    Home(Blasi)

--- vaccination_helper/lib/main.dart
+++ vaccination_helper/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:vaccination_helper/core/redux/store.dart';
 import 'package:vaccination_helper/core/settings/settings_persist.dart';
 import 'package:vaccination_helper/helpers/rest/rest_client.dart';
 import 'package:async_redux/async_redux.dart';
-import 'package:vaccination_helper/pages/home/home.dart';
+import 'package:vaccination_helper/pages/home_page/home.dart';
 import 'package:vaccination_helper/pages/loading_page/loading.dart';
 
 void main() async {
@@ -29,6 +29,6 @@ class VaccinationApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Vaccination Helper', home: Loading());
+    return MaterialApp(title: 'Vaccination Helper', home: Home());
   }
 }
