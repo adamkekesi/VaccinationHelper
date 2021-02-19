661f7ce8ed55eca895d9b9f5282cdf520166e6c1
Balázs Blasek <blasek.balazs@gmail.com>
2021-02-19 13:29:14 +0100

    error

--- vaccination_helper/lib/main.dart
+++ vaccination_helper/lib/main.dart
@@ -5,10 +5,11 @@ import 'package:vaccination_helper/core/redux/store.dart';
 import 'package:vaccination_helper/core/settings/settings_persist.dart';
 import 'package:vaccination_helper/helpers/rest/rest_client.dart';
 import 'package:async_redux/async_redux.dart';
+import 'package:vaccination_helper/pages/home/home.dart';
 
 void main() async {
-  await SettingsPersist.init();
-  await AuthPersist.init();
+//  await SettingsPersist.init();
+//  await AuthPersist.init();
   runApp(MyApp());
 }
 
@@ -16,8 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StoreProvider<AppState>(
-      child:
-          MaterialApp(title: 'Demo App', home: UserExceptionDialog<AppState>()),
+      child: MaterialApp(title: 'Demo App', home: Home()),
       store: store,
     );
   }
