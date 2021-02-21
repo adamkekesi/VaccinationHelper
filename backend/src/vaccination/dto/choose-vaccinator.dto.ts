b58de20f96c45663cbb84e43c05a51b10254d7a4
adamkekesi <kekesiadi@gmail.com>
2021-02-21 09:08:19 +0100

    heroku

--- backend/src/vaccination/dto/choose-vaccinator.dto.ts
+++ backend/src/vaccination/dto/choose-vaccinator.dto.ts
@@ -1,6 +1,5 @@
 import { IsNotEmpty, isNotEmpty } from "class-validator";
 import { JsonProperty, Serializable } from "typescript-json-serializer";
-import { Serializer } from "v8";
 
 @Serializable()
 export default class ChooseVaccinatorDto {
